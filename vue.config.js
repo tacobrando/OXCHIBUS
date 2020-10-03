bf96bf8fde39d34b7cfe68ebabd2d050e096e244
Matthew Lee <matthewytlee2@gmail.com>
2020-10-03 03:12:19 +0100

    push icons

---
+++ vue.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  pwa: {
+    name: 'Oxford Chinese Businesses',
+    short_name: 'Oxchibus',
+    start_url: '.',
+    display: 'standalone',
+    background_color: '#fff',
+    description: 'Oxchibus App',
+    icons: [
+      {
+        src: 'android-chrome-512x512.png',
+        sizes: '512x512',
+        type: 'image/png'
+      },
+      {
+        src: 'android-chrome-192x192.png',
+        sizes: '192x192',
+        type: 'image/png'
+      }
+    ]
+  }
+}
